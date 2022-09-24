4d89582f5d10320bb5e9e02e1f813348f058c742
Jitendra Sharma <jitendra.sharma@intimetec.com>
2022-09-24 13:53:45 +0530

    without error

--- src/test/java/compositionExample/Main.java
+++ src/test/java/compositionExample/Main.java
@@ -1,9 +1,28 @@
 package compositionExample;
-class Main
-{
+class Test {
 
-    public static void main (String[] args)
-    {
-        System.out.println("Hello World");
-    }
+int i;
+
+  // create private constructor
+  private Test () {
+    i = 5;
+  }
+
+  // create a public static method
+  public static void instanceMethod() {
+
+    // create an instance of Test class
+    Test obj = new Test();
+    obj.i = 5;
+  }
+
+}
+
+class Main {
+
+  public static void main(String[] args) {
+
+    // call the instanceMethod()
+    Test.instanceMethod();
+  }
 }
